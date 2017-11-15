b666eeb74202af588a0df41cff40deba3c8aab9d
Surasak Potipipit <SPotipipit@dstworldwideservices.com>
2017-11-15 10:02:12 +0700

    Split component

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <footer class="footer">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-3">
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="container is-fullhd">
+      <section class="hero is-info">
+        <div class="hero-body">
+          <div class="container">
+            <p class="title">
+              Info title
+            </p>
+            <p class="subtitle">
+              Info subtitle
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
