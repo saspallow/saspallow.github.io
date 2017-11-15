825e9ce188a58d47efb33f0462a59c9ef4ea6e7a
Surasak Potipipit <SPotipipit@dstworldwideservices.com>
2017-11-15 10:01:20 +0700

    Initial

---
+++ src/components/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="hero is-dark is-fullheight">
+    <Navbar></Navbar>
+    <Body></Body>
+    <Footer></Footer>
+  </section>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+import Body from '@/components/Body'
+import Footer from '@/components/Footer'
+
+export default {
+  components: {
+    Navbar,
+    Body,
+    Footer
+  }
+}
+</script>
+
