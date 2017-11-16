d5780d1d3e9be149fe759b509dee6c0af2a3e7e8
Surasak Phothiphiphit <hrll0102@hotmail.com>
2017-11-16 10:27:33 +0700

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,4 @@
 <template>
-  <div>
     <footer class="footer notification is-info">
       <div class="container">
         <div class="columns">
@@ -16,11 +15,10 @@
           </div>
           <div class="column is-3">
             <a href="https://bulma.io">
-              <img src="https://bulma.io/images/made-with-bulma--white.png" alt="Made with Bulma" width="128" height="24">
+              <img src="https://bulma.io/images/made-with-bulma--dark.png" alt="Made with Bulma" width="128" height="24">
             </a>
           </div>
         </div>
       </div>
     </footer>
-  </div>
 </template>
